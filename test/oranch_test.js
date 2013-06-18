2970115523e3159450ea65a344d51bc13e9ca508
lewuathe <lewuathe@me.com>
2013-06-18 13:40:55 +0900

    Make vows test codes

--- test/oranch_test.js
+++ test/oranch_test.js
@@ -1,8 +1,59 @@
-var Oranch = require('./oranch.js').Oranch;
+var vows = require('vows');
+var assert = require('assert');
 
-function testTask(line) {
-	console.log("Match: " + line);
+var Oranch = require('../oranch.js').Oranch;
+
+var outs = {};
+function testTask(outName) {
+	return function(line) {
+		outs[outName] += line + '\n';
+	};
 }
 
-var o = new Oranch('*/5 * * * * *', './sample.log', /DEBUG/, testTask, 1000);
+vows.describe('Oranch test').addBatch({
+	'Matching test' : { 
+		topic : function () {
+			outs['match_test'] = '';
+			var options = {};
+			options.schedule = '* * * * * *';
+			options.logfile  = './match_test.log';
+			options.match    = /DEBUG/;
+			options.task     = testTask('match_test');
+			options.onComplete = this.callback;
+			var o = new Oranch(options);
+			o.start();
+			setTimeout(function() {
+				o.stop();
+			}, 2000);
+		},
+		'can grab DEBUG log' : function () {
+			var expect = "Mon Jun 10 2013 11:16:55 GMT+0900 (JST): [DEBUG] - log9\nMon Jun 10 2013 11:16:55 GMT+0900 (JST): [DEBUG] - log15\n";
+			assert.equal(expect, outs['match_test']);
+		}
+	},
+
+	'No matching test' : {
+		topic : function () {
+			outs['nomatch_test'] = '';
+			var options = {};
+			options.schedule = '* * * * * *';
+			options.logfile  = './nomatch_test.log';
+			options.match    = /DEBUG/;
+			options.task     = testTask('nomatch_test');
+			options.onComplete = this.callback;
+			var o = new Oranch(options);
+			o.start();
+			setTimeout(function() {
+				o.stop();
+			}, 2000);
+		},
+		'cannot grab DEBUG log' : function () {
+			var expect = '';
+			assert.equal(expect, outs['nomatch_test']);
+		}
+	}
+}).run();
+
+
+
 
