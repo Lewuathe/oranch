8587732b91c839d12f86ca8f23a795b39d1e6a9f
lewuathe <lewuathe@me.com>
2013-06-18 18:45:52 +0900

    Add test case

--- test/oranch_test.js
+++ test/oranch_test.js
@@ -1,6 +1,5 @@
 var vows = require('vows');
 var assert = require('assert');
-
 var Oranch = require('../oranch.js').Oranch;
 
 var outs = {};
@@ -16,7 +15,7 @@ vows.describe('Oranch test').addBatch({
 			outs['match_test'] = '';
 			var options = {};
 			options.schedule = '* * * * * *';
-			options.logfile  = './match_test.log';
+			options.logfile  = __dirname + '/match_test.log';
 			options.match    = /DEBUG/;
 			options.task     = testTask('match_test');
 			options.onComplete = this.callback;
@@ -32,18 +31,39 @@ vows.describe('Oranch test').addBatch({
 		}
 	},
 
+
+	'Unset matching options': {
+		topic : function () {
+			outs['unsetmatch_test'] = '';
+			var options = {};
+			options.schedule = '* * * * * *';
+			options.logfile  = __dirname + '/unsetmatch_test.log';
+			options.task     = testTask('unsetmatch_test');
+			options.onComplete = this.callback;
+			var o = new Oranch(options);
+			o.start();
+			setTimeout(function () {
+				o.stop();
+			}, 2000);
+		},
+		'grab all log' : function () {
+			var expect = 'Mon Jun 10 2013 11:16:55 GMT+0900 (JST): [INFO] - log1\nMon Jun 10 2013 11:16:55 GMT+0900 (JST): [INFO] - log2\nMon Jun 10 2013 11:16:55 GMT+0900 (JST): [INFO] - log3\nMon Jun 10 2013 11:16:55 GMT+0900 (JST): [INFO] - log4\nMon Jun 10 2013 11:16:55 GMT+0900 (JST): [INFO] - log5\nMon Jun 10 2013 11:16:55 GMT+0900 (JST): [INFO] - log6\nMon Jun 10 2013 11:16:55 GMT+0900 (JST): [INFO] - log7\nMon Jun 10 2013 11:16:55 GMT+0900 (JST): [INFO] - log8\nMon Jun 10 2013 11:16:55 GMT+0900 (JST): [DEBUG] - log9\nMon Jun 10 2013 11:16:55 GMT+0900 (JST): [INFO] - log10\nMon Jun 10 2013 11:16:55 GMT+0900 (JST): [INFO] - log11\nMon Jun 10 2013 11:16:55 GMT+0900 (JST): [INFO] - log12\nMon Jun 10 2013 11:16:55 GMT+0900 (JST): [INFO] - log13\nMon Jun 10 2013 11:16:55 GMT+0900 (JST): [INFO] - log14\nMon Jun 10 2013 11:16:55 GMT+0900 (JST): [DEBUG] - log15\n';
+			assert.equal(expect, outs['unsetmatch_test']);
+		}
+	},
+
 	'No matching test' : {
 		topic : function () {
 			outs['nomatch_test'] = '';
 			var options = {};
 			options.schedule = '* * * * * *';
-			options.logfile  = './nomatch_test.log';
+			options.logfile  = __dirname + '/nomatch_test.log';
 			options.match    = /DEBUG/;
 			options.task     = testTask('nomatch_test');
 			options.onComplete = this.callback;
 			var o = new Oranch(options);
 			o.start();
-			setTimeout(function() {
+			setTimeout(function () {
 				o.stop();
 			}, 2000);
 		},
@@ -51,8 +71,27 @@ vows.describe('Oranch test').addBatch({
 			var expect = '';
 			assert.equal(expect, outs['nomatch_test']);
 		}
+	},
+
+	'No schedule' : {
+		topic : function () {
+			outs['noschedule_test'] = '';
+			var options = {};
+			//options.schedule = '* * * * * *';
+			options.logfile  = __dirname + '/noschedule.log';
+			options.match    = /DEBUG/;
+			options.task     = testTask('noschedule_test');
+			//options.onComplete = this.callback;
+			return new Oranch(options);
+		},
+		'schedule object' : function (topic) {
+			assert.throws(function () { 
+				topic.start();
+			}, Error);
+		}
 	}
-}).run();
+
+}).export(module);
 
 
 
