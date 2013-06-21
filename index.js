5c3d37d9fc8c85c619623511f8c6d34df845576a
lewuathe <lewuathe@me.com>
2013-06-21 23:10:15 +0900

    alreadyRead file output

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var cronJob = require('cron').CronJob;
 var fs = require('fs');
+var path = require('path');
 
 var alreadyRead = 0;
 
@@ -13,6 +14,8 @@ function isEnd(line) {
 	}
 }
 
+
+
 // Initialization of buffer of null character
 function initBuffer(buf, size) {
 	for (var i = 0; i < size; i++) {
@@ -21,15 +24,16 @@ function initBuffer(buf, size) {
 }
 
 function Oranch(options) {
-	var self        = this;
-	self.schedule   = options.schedule;
-	self.logfile    = options.logfile;
-	self.match      = options.match ? options.match : /.*/;
-	self.task       = options.task;
-	self.bufSize    = options.bufSize ? options.bufSize : 1000;
-	self.onComplete = options.onComplete;
-	self.job        = new cronJob(self.schedule, grabLog, self.onComplete, false);
-	self.job.oranch = self;
+	var self         = this;
+	self.schedule    = options.schedule;
+	self.logfile     = options.logfile;
+	self.match       = options.match ? options.match : /.*/;
+	self.task        = options.task;
+	self.bufSize     = options.bufSize ? options.bufSize : 1000;
+	self.onComplete  = options.onComplete;
+	self.job         = new cronJob(self.schedule, grabLog, self.onComplete, false);
+	self.job.oranch  = self;
+	self.alreadyRead = initAlready(self.logfile);
 }
 
 Oranch.prototype.start = function() {
@@ -39,6 +43,7 @@ Oranch.prototype.start = function() {
 
 Oranch.prototype.stop = function() {
 	var self = this;
+	writeAlready(alreadyFilePath(self.logfile), self.alreadyRead);
 	self.job.stop();
 }
 
@@ -56,7 +61,7 @@ function grabLog() {
 		var buf = new Buffer(bufSize);
 		initBuffer(buf, bufSize);
 		var offset = 0;
-		fs.read(fd, buf, offset, bufSize, alreadyRead, function(err, bytesRead, buffer) {
+		fs.read(fd, buf, offset, bufSize, oranch.alreadyRead, function(err, bytesRead, buffer) {
 			var lines = buffer.toString().split('\n');
 			for( var lineIndex = 0; lineIndex < lines.length-1; lineIndex++ ) {
 				var curLine = lines[lineIndex];
@@ -66,12 +71,35 @@ function grabLog() {
 				if (curLine.match(match)) {
 					task(curLine);
 				}
-				alreadyRead += lines[lineIndex].length + 1;
+				oranch.alreadyRead += lines[lineIndex].length + 1;
 			}
 		});
 	});
 }
 
+function alreadyFilePath(logfile) {
+	var basename = path.basename(logfile);
+	return __dirname + '/alreadys/' + basename + '.already';
+}
+
+function initAlready(logfile) {
+	var alreadyFile = alreadyFilePath(logfile);
+	if (fs.existsSync(alreadyFile)) {
+		return readAlready(alreadyFile);
+	}
+	else {
+		writeAlready(alreadyFile, 0);
+		return 0;
+	}
+}
+
+function writeAlready (alreadyFile, alreadyRead) {
+	fs.writeFileSync(alreadyFile , alreadyRead + '');
+}
+
+function readAlready (alreadyFile) {
+	return parseInt(fs.readFileSync(alreadyFile));
+}
 
 
 exports.Oranch = Oranch;
